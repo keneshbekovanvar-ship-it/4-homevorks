19dded0571c93aacbc42308d2ba441d3f9875e29
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-01-30 00:17:01 +0600

    hw1

---
+++ book/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello_world_view(request):
+    if request.method == "GET":
+        return HttpResponse("Hello World")
