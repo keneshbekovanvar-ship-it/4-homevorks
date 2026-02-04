46fa6fa13dda119ea82a97620b1ff40018e4a822
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-02-04 20:30:12 +0600

    hw2

--- book/views.py
+++ book/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book/book_list.html', {'books': books})
 
-def book_detail(request, id):
-    book = Book.objects.get(id=id)
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     return render(request, 'book/book_detail.html', {'book': book})
