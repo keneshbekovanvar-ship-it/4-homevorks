beba3f267007cb87a8ccf4c3ae45a76d84ad8fce
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-02-08 14:36:36 +0600

    исапрвления дз 3

--- relation_db/models.py
+++ relation_db/models.py
@@ -4,8 +4,17 @@ from django.db import models
 class TouristCategory(models.Model):
     name = models.CharField(max_length=50)
 
-    def __str__(self):
+    def str(self):
         return self.name
+
+
+
+class Tour(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+
+    def str(self):
+        return self.title
 
 
 class Person(models.Model):
@@ -13,10 +22,29 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.PositiveIntegerField()
 
-    categories = models.ManyToManyField(
-        TouristCategory,
-        related_name='persons'
+    tour = models.OneToOneField(
+        Tour,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
+
+    categories = models.ManyToManyField(TouristCategory)
 
     def str(self):
         return f"{self.first_name} {self.last_name}"
+
+
+
+
+class Review(models.Model):
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"Отзыв о {self.person.first_name}"
